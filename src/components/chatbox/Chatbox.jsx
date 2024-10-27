35d362a61a6d23ef3ccfcea36ba60d8cf571df00
Kunal Sunil Koli <kunalkoli537@gmail.com>
2024-10-27 19:16:23 +0530

    noder server setup

--- src/components/chatbox/Chatbox.jsx
+++ src/components/chatbox/Chatbox.jsx
@@ -4,21 +4,20 @@ import './Chatbox.css';
 function Chatbox() {
   return (
     <>
-    <div >
+      <div >
         <div className='container '>
-           <div className='message right'>Kunal: hey</div>
-           <div className='message left'>param: Hii</div>
+          <div className='message right'>Kunal: hey</div>
+          <div className='message left'>param: Hii</div>
         </div>
 
         <div className="send mt-1">
-            <form action="#" id='msgsend'>
-                <input className='inputmsg border border-black w-[65%]' type="text" name='inputmsg' id='inputmsg' />
-                <button className='sendbtn ml-2 ' type='submit'>send</button>
-
-            </form>
+          <form action="#" id='msgsend'>
+            <input className='inputmsg border border-black w-[65%]' type="text" name='inputmsg' id='inputmsg' />
+            <button className='sendbtn ml-2 ' type='submit'>send</button>
+          </form>
         </div>
 
-    </div>
+      </div>
     </>
   )
 }
